bf2ab76d6168537065c0e878608986361e2dc035
Osborn Kwarteng Adu <osborn.kwarteng.adu@meltwater.org>
2013-09-11 05:59:24 +0000

    I deleted some few unnecessary lines

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,3 @@ $(document).ready(function(){
 	});
 });
 
-
-
-function getPicture(){
-	var styles = new Array("#picture-one", "#picture-two", "#picture-three");
-
-
-	for (var i=0; i<styles.length; i++){
-		$(styles[i]).click(function(){
-			$('#canvas').html(styles[0]);
-		});
-	}
-}
