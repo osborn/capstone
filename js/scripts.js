c15b562d60f29f61cbbe662203db3d69b7265366
Osborn Kwarteng Adu <osborn.kwarteng.adu@meltwater.org>
2013-09-13 11:06:11 +0000

    added a gradient style to the container

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
 	$('.carousel-inner').find('img').click(function(){
 		var clone = $(this).clone();
 		$('#canvas').html(clone);
+		$('#canvas').append('<p>$15</p>');
 	});
 
 	$('#color1').click(function(){
 		$('#canvas').html('<img src="img/picture-three-colored.png">');
+		$('#canvas').append('<p>$15</p>');
 	});
 });
 
